dbb7648173a4cef01f2f386701d4330701ab184b
misraved <ved@turbot.com>
2023-04-27 15:56:43 +0530

    Add env0_user table

--- env0/plugin.go
+++ env0/plugin.go
@@ -24,6 +24,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"env0_organization": tableEnv0Organization(ctx),
 			"env0_project":      tableenv0Project(ctx),
 			"env0_team":         tableEnv0Team(ctx),
+			"env0_user":         tableEnv0User(ctx),
 		},
 	}
 	return p
