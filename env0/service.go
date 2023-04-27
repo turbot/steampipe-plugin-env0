5f3769454104ae8b561b11bac8610ef064a4c8cf
misraved <ved@turbot.com>
2023-04-27 11:16:00 +0530

    Tidy service.go file

--- env0/service.go
+++ env0/service.go
@@ -3,9 +3,7 @@ package env0
 import (
 	"context"
 	"errors"
-	"log"
 	"os"
-	"time"
 
 	env0CLient "github.com/env0/terraform-provider-env0/client"
 	"github.com/env0/terraform-provider-env0/client/http"
@@ -54,45 +52,12 @@ func connect(ctx context.Context, d *plugin.QueryData) (env0CLient.ApiClientInte
 		return nil, errors.New("organization_id must be configured")
 	}
 
-	apiEndpoint := "https://api.env0.com/"
-	// organizationId := "fe8c0666-b2ed-498f-a94b-3f57e1923021"
-
-	isIntegrationTest := false
-
-	restyClient := resty.New()
-
-	restyClient.
-		SetRetryCount(5).
-		SetRetryWaitTime(time.Second).
-		SetRetryMaxWaitTime(time.Second * 5).
-		AddRetryCondition(func(r *resty.Response, err error) bool {
-			if r == nil {
-				// No response. Possiblly a networking issue (E.g. DNS lookup failure).
-				log.Printf("[WARN] No response, retrying request: %s %s", r.Request.Method, r.Request.URL)
-				return true
-			}
-
-			// When running integration tests 404 may occur due to "database eventual consistency".
-			// Retry when there's a 5xx error. Otherwise do not retry.
-			if r.StatusCode() >= 500 || isIntegrationTest && r.StatusCode() == 404 {
-				log.Printf("[WARN] Received %d status code, retrying request: %s %s", r.StatusCode(), r.Request.Method, r.Request.URL)
-				return true
-			}
-
-			if r.StatusCode() == 200 && isIntegrationTest && r.String() == "[]" {
-				log.Printf("[WARN] Received an empty list for an integration test, retrying request: %s %s", r.Request.Method, r.Request.URL)
-				return true
-			}
-
-			return false
-		})
-
 	httpClient, err := http.NewHttpClient(http.HttpClientConfig{
 		ApiKey:      api_key,
 		ApiSecret:   api_secret,
-		ApiEndpoint: apiEndpoint,
+		ApiEndpoint: "https://api.env0.com/",
 		UserAgent:   "",
-		RestClient:  restyClient,
+		RestClient:  resty.New(),
 	})
 	if err != nil {
 		plugin.Logger(ctx).Error("Error in creating the client", err)
